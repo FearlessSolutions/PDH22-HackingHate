8c9b18ddefae09a0422032bbce133828a4114510
Evan Rittenhouse <erittenhouse@fearless.tech>
2022-12-15 13:49:52 -0500

    Publish updated message extractor function

--- CommonMessages/src/main/kotlin/Messages.kt
+++ CommonMessages/src/main/kotlin/Messages.kt
@@ -1,3 +1,4 @@
 package tech.fearless.purpledatahacks
 
+data class ClassificationMessage(val sendingUser: String, val message: String)
 data class PotentiallySexistMessage(val sendingUser: String, val message: String, val confidence: Float)
--- SlackMessageExtractor/src/main/kotlin/MessagesExtractorFunction.kt
+++ SlackMessageExtractor/src/main/kotlin/MessagesExtractorFunction.kt
@@ -4,13 +4,29 @@ import com.google.cloud.functions.HttpFunction
 import com.google.cloud.functions.HttpRequest
 import com.google.cloud.functions.HttpResponse
 import com.slack.api.Slack
+import com.slack.api.methods.MethodsClient
 import mu.KotlinLogging
+import tech.fearless.purpledatahacks.ClassificationMessage
 import java.io.IOException
 
 data class ChannelRequest(val channel: String)
 
 class MessagesExtractorFunction : HttpFunction {
-    val logger = KotlinLogging.logger {}
+    private val logger = KotlinLogging.logger {}
+
+    private fun fetchUserData(userId: String, slackClient: MethodsClient, memoryMap: MutableMap<String, String>): String {
+        return memoryMap.getOrPut(userId) {
+            val foundUser = slackClient.usersInfo { req ->
+                req.user(userId)
+            }
+
+            if (!foundUser.isOk) {
+                throw IllegalArgumentException("Could not find user: $userId")
+            }
+
+            "${foundUser.user.realName} (${foundUser.user.profile.displayName})"
+        }
+    }
 
     @Throws(IOException::class)
     override fun service(request: HttpRequest, response: HttpResponse) {
@@ -41,11 +57,19 @@ class MessagesExtractorFunction : HttpFunction {
         }
 
         // Read messages
+        val userIdToName = mutableMapOf<String, String>()
+
         var historyPage = slackClient.conversationsHistory { req ->
             req.channel(parsedRequest.channel)
                 .limit(20)
         }
-        val foundMessages = historyPage.messages.map { it.text }.toMutableList()
+        val foundMessages = historyPage.messages
+            .filter { it.subtype.isNullOrEmpty() }
+            .map {
+                val user = fetchUserData(it.user, slackClient, userIdToName)
+                ClassificationMessage(user, it.text)
+            }
+            .toMutableList()
 
         while (historyPage.isHasMore) {
             historyPage = slackClient.conversationsHistory { req ->
@@ -54,9 +78,16 @@ class MessagesExtractorFunction : HttpFunction {
                     .limit(20)
             }
 
-            foundMessages.addAll(historyPage.messages.map { it.text })
+            foundMessages.addAll(historyPage.messages
+                .filter { it.subtype.isNullOrEmpty() }
+                .map {
+                    val user = fetchUserData(it.user, slackClient, userIdToName)
+                    ClassificationMessage(user, it.text)
+                }
+            )
         }
 
+        logger.info { "Passing along ${foundMessages.size} messages..." }
         jsonParser.writeValue(response.writer, foundMessages)
     }
 }
