169d043d815a7928864d25e432fcb0155a2c4361
Evan Rittenhouse <erittenhouse@fearless.tech>
2024-03-26 04:19:45 -0400

    Add prepared batch dataset for model evaluation

--- DataPrep/src/main/kotlin/Main.kt
+++ DataPrep/src/main/kotlin/Main.kt
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.attribute.FileAttribute
+import kotlin.io.path.*
 
 interface ClassifiedText {
     val text: String
@@ -27,6 +31,9 @@ data class OriginalCommentDataGesis(val id: Int, val dataset: String, override v
 data class Classification(val displayName: String)
 data class PreparedData(val textContent: String, val classificationAnnotation: Classification)
 
+data class BatchPredictionEntry(val content: String, val classificationLabel: String, val mimeType: String = "text/plain")
+
+// This function creates a training dataset
 inline fun <reified  InputSchema: ClassifiedText> csvToJsonL(
     inputFile: File,
     outputFile: File,
@@ -53,16 +60,68 @@ inline fun <reified  InputSchema: ClassifiedText> csvToJsonL(
     }
 }
 
+// This function creates a batch prediction dataset that can be used in evaluations
+inline fun <reified InputSchema: ClassifiedText> csvToBatchPredictionJsonL(
+    inputFile: File,
+    outputTextFileFolder: Path,
+    outputFile: File,
+    csvMapper: CsvMapper,
+    jsonMapper: ObjectMapper,
+    cloudStorageBucketPrefix: String,
+    filter: (InputSchema) -> Boolean = { _ -> true }
+) {
+    val ingestSchema = csvMapper.schemaFor(InputSchema::class.java)
+    val reader = csvMapper.readerFor(InputSchema::class.java)
+        .with(ingestSchema)
+        .readValues<InputSchema>(inputFile)
+
+    if (!outputFile.exists()) outputFile.createNewFile()
+    outputFile.writer().use { writer ->
+        for ((index, comment) in reader.withIndex()) {
+            if (!filter(comment)) continue
+            val formattedNumString = "%06d".format(index)
+            val fileName = "batch_data_$formattedNumString.txt"
+            val outputPath = outputTextFileFolder.resolve(fileName)
+
+            if (!outputTextFileFolder.exists()) {
+                outputTextFileFolder.createDirectory()
+            }
+            if (!outputPath.exists()) {
+                outputPath.createFile()
+            }
+            outputPath.writeText(comment.text)
+
+            val jsonified = BatchPredictionEntry(
+                content = "$cloudStorageBucketPrefix/$fileName",
+                classificationLabel = if (comment.isSexist) "sexist" else "non-sexist",
+            )
+
+            writer.write(jsonMapper.writeValueAsString(jsonified) + "\n")
+        }
+    }
+}
+
 fun main() {
     val originalInputIsep = File("./DataPrep/src/main/resources/ISEP Sexist Data Labeling.csv")
     val originalInputGesis = File("./DataPrep/src/main/resources/GESIS_sexism_data.csv")
 
     val outputFileIsep = File("./DataPrep/src/main/resources/training_data.jsonl")
     val outputFileGesis = File("./DataPrep/src/main/resources/training_data_gesis.jsonl")
+    val outputGesisContentFolder = Paths.get("./DataPrep/src/main/resources/batch_prediction_gesis_content/")
+    val outputFileGesisBatch = File("./DataPrep/src/main/resources/batch_prediction_gesis.jsonl")
 
     val jsonMapper = jacksonObjectMapper()
     val csvMapper = CsvMapper().registerKotlinModule() as CsvMapper
 
     csvToJsonL<OriginalCommentDataIsep>(originalInputIsep, outputFileIsep, csvMapper, jsonMapper)
+    // We're excluding entries in the Gesis dataset which refer to other entries. Trying to keep it simple.
     csvToJsonL<OriginalCommentDataGesis>(originalInputGesis, outputFileGesis, csvMapper, jsonMapper) { it.of_id == -1 }
+    csvToBatchPredictionJsonL<OriginalCommentDataGesis>(
+        originalInputGesis,
+        outputGesisContentFolder,
+        outputFileGesisBatch,
+        csvMapper,
+        jsonMapper,
+        "gs://hacking-hate-speech-raw-data/gesis_batch_source"
+    ) { it.of_id == -1 }
 }
