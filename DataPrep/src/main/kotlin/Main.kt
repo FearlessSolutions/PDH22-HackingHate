841a52d603eb8d5f9876a3be56b0a1442e889052
Evan Rittenhouse <erittenhouse@fearless.tech>
2024-03-26 01:15:56 -0400

    Add new dataset for additional evaluation and model refinement

--- DataPrep/src/main/kotlin/Main.kt
+++ DataPrep/src/main/kotlin/Main.kt
@@ -1,35 +1,68 @@
 import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.csv.CsvMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
 
+interface ClassifiedText {
+    val text: String
+    val isSexist: Boolean
+}
+
 @JsonPropertyOrder("sentences", "label")
-data class OriginalCommentData(val sentences: String, val label: Int)
+data class OriginalCommentDataIsep(val sentences: String, val label: Int): ClassifiedText {
+    override val text: String
+        get() = sentences
+    override val isSexist: Boolean
+        get() = label == 1
+}
+
+@JsonPropertyOrder("id", "dataset", "text", "toxicity", "sexist", "of_id")
+data class OriginalCommentDataGesis(val id: Int, val dataset: String, override val text: String, val toxicity: Double, val sexist: Boolean, val of_id: Int): ClassifiedText {
+    override val isSexist: Boolean
+        get() = sexist
+}
 
 data class Classification(val displayName: String)
 data class PreparedData(val textContent: String, val classificationAnnotation: Classification)
 
-fun main() {
-    val originalInput = File("./DataPrep/src/main/resources/ISEP Sexist Data Labeling.csv")
-    val outputFile = File("./DataPrep/src/main/resources/training_data.jsonl")
-    val jsonMapper = jacksonObjectMapper()
-    val csvMapper = CsvMapper().registerKotlinModule() as CsvMapper
-
-    val ingestSchema = csvMapper.schemaFor(OriginalCommentData::class.java)
-    val reader = csvMapper.readerFor(OriginalCommentData::class.java)
+inline fun <reified  InputSchema: ClassifiedText> csvToJsonL(
+    inputFile: File,
+    outputFile: File,
+    csvMapper: CsvMapper,
+    jsonMapper: ObjectMapper,
+    filter: (InputSchema) -> Boolean = { _ -> true }
+) {
+    val ingestSchema = csvMapper.schemaFor(InputSchema::class.java)
+    val reader = csvMapper.readerFor(InputSchema::class.java)
         .with(ingestSchema)
-        .readValues<OriginalCommentData>(originalInput)
+        .readValues<InputSchema>(inputFile)
 
     if (!outputFile.exists()) outputFile.createNewFile()
     outputFile.writer().use { writer ->
         for (comment in reader) {
+            if (!filter(comment)) continue
             val jsonified = PreparedData(
-                textContent = comment.sentences,
-                classificationAnnotation = Classification(if (comment.label == 1) "sexist" else "non-sexist")
+                textContent = comment.text,
+                classificationAnnotation = Classification(if (comment.isSexist) "sexist" else "non-sexist")
             )
 
             writer.write(jsonMapper.writeValueAsString(jsonified) + "\n")
         }
     }
+}
+
+fun main() {
+    val originalInputIsep = File("./DataPrep/src/main/resources/ISEP Sexist Data Labeling.csv")
+    val originalInputGesis = File("./DataPrep/src/main/resources/GESIS_sexism_data.csv")
+
+    val outputFileIsep = File("./DataPrep/src/main/resources/training_data.jsonl")
+    val outputFileGesis = File("./DataPrep/src/main/resources/training_data_gesis.jsonl")
+
+    val jsonMapper = jacksonObjectMapper()
+    val csvMapper = CsvMapper().registerKotlinModule() as CsvMapper
+
+    csvToJsonL<OriginalCommentDataIsep>(originalInputIsep, outputFileIsep, csvMapper, jsonMapper)
+    csvToJsonL<OriginalCommentDataGesis>(originalInputGesis, outputFileGesis, csvMapper, jsonMapper) { it.of_id == -1 }
 }
---
+++ DataPrep/src/main/resources/CITATIONS.md
@@ -0,0 +1,4 @@
+# Dataset citations
+
+Samory, Mattia (2021). The 'Call me sexist but' Dataset (CMSB). GESIS, Cologne. Data File Version 1.0.0, https://doi.org/10.7802/2251.
+https://www.kaggle.com/datasets/dgrosz/sexist-workplace-statements
